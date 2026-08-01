60b3eb293113954428ec7bfa4c924885f9aa47d6
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 19:25:46 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,35 @@
 import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
+alert("login.js loaded");
+
 const loginBtn = document.getElementById("loginBtn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const msg = document.getElementById("msg");
 
-loginBtn.addEventListener("click", async () => {
-  if (email.value === "" || password.value === "") {
-    msg.innerHTML = "Please enter email and password.";
-    return;
-  }
+if (!loginBtn) {
+    alert("Login button not found!");
+} else {
+    loginBtn.addEventListener("click", async () => {
+        if (email.value.trim() === "" || password.value.trim() === "") {
+            msg.innerHTML = "Please enter email and password.";
+            return;
+        }
 
-  try {
-    await signInWithEmailAndPassword(auth, email.value, password.value);
-    window.location.href = "dashboard.html";
-  } catch (error) {
-    msg.innerHTML = error.message;
-  }
-});
+        try {
+            await signInWithEmailAndPassword(
+                auth,
+                email.value.trim(),
+                password.value
+            );
+
+            alert("Login Successful");
+            window.location.href = "dashboard.html";
+
+        } catch (error) {
+            alert(error.code);
+            msg.innerHTML = error.message;
+        }
+    });
+}
