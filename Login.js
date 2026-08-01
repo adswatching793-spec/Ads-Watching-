8036d7632fec1481c348c0788e5f585a555b6358
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 19:01:44 +0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,1 +1,21 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const msg = document.getElementById("msg");
+
+loginBtn.addEventListener("click", async () => {
+  if (email.value === "" || password.value === "") {
+    msg.innerHTML = "Please enter email and password.";
+    return;
+  }
+
+  try {
+    await signInWithEmailAndPassword(auth, email.value, password.value);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    msg.innerHTML = error.message;
+  }
+});
