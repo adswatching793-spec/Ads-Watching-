fd888ca27a622724651414e9fc97ca3e83b2b69c
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 20:50:20 +0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,14 @@
-const userName = localStorage.getItem("userName");
-const userEmail = localStorage.getItem("userEmail");
-const balance = localStorage.getItem("balance");
+import { auth, db } from "./firebase.js";
+
+import {
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+
 
 
 const nameElement = document.querySelector(".profile-box h2");
@@ -8,28 +16,65 @@ const emailElement = document.querySelector(".profile-box p");
 const balanceElement = document.getElementById("balance");
 
 
-// Show User Name
-if(userName && nameElement){
-    nameElement.innerHTML = "Welcome " + userName + " 👋";
+
+onAuthStateChanged(auth, async (user)=>{
+
+
+if(user){
+
+
+const userRef = doc(db,"users",user.uid);
+
+const userSnap = await getDoc(userRef);
+
+
+
+if(userSnap.exists()){
+
+
+const data = userSnap.data();
+
+
+if(nameElement){
+nameElement.innerHTML = "Welcome " + data.name + " 👋";
 }
 
 
-// Show Email
-if(userEmail && emailElement){
-    emailElement.innerHTML = userEmail;
+if(emailElement){
+emailElement.innerHTML = data.email;
 }
 
 
-// Show Balance
-if(balance && balanceElement){
-    balanceElement.innerHTML = "PKR " + balance;
+if(balanceElement){
+balanceElement.innerHTML = "PKR " + data.balance;
 }
+
+
+
+}
+
+
+}else{
+
+
+window.location.href="login.html";
+
+
+}
+
+
+
+});
+
+
+
+
 function logout(){
 
-    localStorage.removeItem("userEmail");
-    localStorage.removeItem("userName");
-    localStorage.removeItem("balance");
+localStorage.removeItem("userEmail");
+localStorage.removeItem("userName");
+localStorage.removeItem("balance");
 
-    window.location.href = "login.html";
+window.location.href="login.html";
 
 }
