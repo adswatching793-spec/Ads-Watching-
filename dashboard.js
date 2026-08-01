50f5034413ceb38290099510b594206fbb842e37
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 20:11:03 +0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,26 @@
+const userName = localStorage.getItem("userName");
+const userEmail = localStorage.getItem("userEmail");
+const balance = localStorage.getItem("balance");
 
+
+const nameElement = document.querySelector(".profile-box h2");
+const emailElement = document.querySelector(".profile-box p");
+const balanceElement = document.getElementById("balance");
+
+
+// Show User Name
+if(userName && nameElement){
+    nameElement.innerHTML = "Welcome " + userName + " 👋";
+}
+
+
+// Show Email
+if(userEmail && emailElement){
+    emailElement.innerHTML = userEmail;
+}
+
+
+// Show Balance
+if(balance && balanceElement){
+    balanceElement.innerHTML = "PKR " + balance;
+}
