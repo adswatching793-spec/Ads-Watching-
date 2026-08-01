7976c5389377538edb31c6c693cb772be7eecc39
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 20:05:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-document.querySelector(".login").addEventListener("click", function () {
-    window.location.href = "login.html";
-});
+// Home Page Buttons
 
-document.querySelector(".register").addEventListener("click", function () {
-    window.location.href = "register.html";
-});
+let loginButton = document.querySelector(".login");
+let registerButton = document.querySelector(".register");
+
+
+if(loginButton){
+    loginButton.addEventListener("click", function(){
+        window.location.href = "login.html";
+    });
+}
+
+
+if(registerButton){
+    registerButton.addEventListener("click", function(){
+        window.location.href = "register.html";
+    });
+}
+
+
+// Save User Data Function
+
+function saveUser(name,email){
+
+    localStorage.setItem("userName", name);
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("balance", "0");
+
+}
+
+
+// Get User Data
+
+function getUser(){
+
+    return {
+        name: localStorage.getItem("userName"),
+        email: localStorage.getItem("userEmail"),
+        balance: localStorage.getItem("balance")
+    };
+
+}
