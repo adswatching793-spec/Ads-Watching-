9f6f65c0763c2a4ada828fd1d132cce35a14fc56
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 20:09:20 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,35 +1,63 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+import { 
+signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
-alert("login.js loaded");
 
 const loginBtn = document.getElementById("loginBtn");
+
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+
 const msg = document.getElementById("msg");
 
-if (!loginBtn) {
-    alert("Login button not found!");
-} else {
-    loginBtn.addEventListener("click", async () => {
-        if (email.value.trim() === "" || password.value.trim() === "") {
-            msg.innerHTML = "Please enter email and password.";
-            return;
-        }
 
-        try {
-            await signInWithEmailAndPassword(
-                auth,
-                email.value.trim(),
-                password.value
-            );
+loginBtn.addEventListener("click", async () => {
 
-            alert("Login Successful");
-            window.location.href = "dashboard.html";
 
-        } catch (error) {
-            alert(error.code);
-            msg.innerHTML = error.message;
-        }
-    });
-}
+    if(email.value.trim()==="" || password.value.trim()===""){
+
+        msg.innerHTML="Please enter email and password.";
+        return;
+
+    }
+
+
+    try{
+
+
+        await signInWithEmailAndPassword(
+            auth,
+            email.value.trim(),
+            password.value
+        );
+
+
+        // Save login user info
+        localStorage.setItem(
+            "userEmail",
+            email.value.trim()
+        );
+
+
+        msg.innerHTML="Login Successful ✅";
+
+
+        setTimeout(()=>{
+
+            window.location.href="dashboard.html";
+
+        },1000);
+
+
+
+    }catch(error){
+
+
+        msg.innerHTML=error.message;
+
+
+    }
+
+
+});
