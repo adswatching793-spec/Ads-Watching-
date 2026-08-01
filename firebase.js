5439f8ae2db9434904bf2bb58ed7292325262ae5
adswatching793-spec <adswatching793@gmail.com>
2026-08-01 18:52:13 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
 
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBkDYyByiFAq0sco4_tQwlVLT5xYcEAJLM",
+  authDomain: "ads-watching-3990f.firebaseapp.com",
+  projectId: "ads-watching-3990f",
+  storageBucket: "ads-watching-3990f.firebasestorage.app",
+  messagingSenderId: "1096574398103",
+  appId: "1:1096574398103:web:1a64a516b87e0e009683d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
